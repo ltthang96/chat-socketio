f244d41a3425533b749e649926556bd94c8c2719
ltthang96 <luctoanthang96@gmail.com>
2017-09-10 23:50:21 +0700

    initial commit

---
+++ SERVER.js
@@ -0,0 +1,98 @@
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(3000);
+
+//mang user
+var mangUsers=[]; 
+
+io.on("connection",function(socket){
+	console.log("Có "+socket.id+ " kết nối đến server");
+
+	//nhan username tu client
+	socket.on("Client-send-username",function(data){
+		if(mangUsers.indexOf(data)>=0){
+			//fail
+			socket.emit("server-send-dki-thatbai");	
+			}
+			//success
+			else{
+				mangUsers.push(data);
+				socket.Username = data; //tao username
+				socket.emit("server-send-dki-thanhcong",data);				
+				io.sockets.emit("server-send-ds-user", mangUsers);
+			}
+		}); 
+
+	socket.on("disconnect",function(){
+		mangUsers.splice(
+			mangUsers.indexOf(socket.Username),1
+			);
+		socket.broadcast.emit("server-send-ds-user",mangUsers);
+	});
+
+	
+	socket.on("logout",function(){
+		mangUsers.splice(
+			mangUsers.indexOf(socket.Username),1
+			);
+		socket.broadcast.emit("server-send-ds-user",mangUsers);
+	});
+
+	socket.on("leave-room",function(){
+		socket.leave("Room 1");
+		console.log(socket.adapter.rooms);
+	});
+
+	socket.on("user-send-messages",function(data){
+		io.sockets.emit("server-send-messages", {un:socket.Username, nd:data});
+	});
+
+	socket.on("chatting",function(){
+		var s = socket.Username+" đang nhập...";
+		socket.broadcast.emit("dang-nhap",s);
+	});
+
+	socket.on("offchat",function(){
+		io.sockets.emit("stop-chat");
+	});
+
+	socket.on("join-room1",function(){
+		socket.join("Room 1");
+		socket.Phong="Room 1";
+		socket.emit("server-send-room","Room 1")
+		console.log(socket);
+		console.log(socket.adapter.rooms);
+	});
+
+	// socket.on("join-room2",function(){
+	// 	socket.join("room2");
+	// });
+
+	// socket.on("join-room3",function(){
+	// 	socket.join("room3");
+	// });
+	
+	socket.on("user-send-messages-room",function(data){
+		io.sockets.in(socket.Phong).emit("server-chat-room",{un:socket.Username, nd:data});
+	});
+
+	socket.on("room-chatting",function(){
+		var s = socket.Username+" đang nhập...";
+		socket.broadcast.emit("room-dang-nhap",s);
+	});
+
+	socket.on("room-offchat",function(){
+		io.sockets.emit("room-stop-chat");
+	});
+
+});
+
+app.get("/", function(req,res){
+	res.render("trangchu");
+});
---
+++ public/xuly.js
@@ -0,0 +1,128 @@
+var socket = io("http://localhost:3000");
+
+//server>client
+socket.on("server-send-dki-thatbai",function(){
+	alert("Duplicate username")
+});
+
+//send ds user
+socket.on("server-send-ds-user",function(data){
+	$("#boxContent").html("");
+	data.forEach(function(i){
+		$("#boxContent").append("<div class='useronline'>" + i+ "</div>");	
+	});
+});
+
+//tbao dki thanh cong, hide loginform
+socket.on("server-send-dki-thanhcong",function(data){
+	alert("Registration successful");
+	$("#currentUser").html(data);
+	$("#loginForm").hide(2000);
+	$("#chatForm").show(1000);
+	$("#listMessagesRoom").hide();
+	$("#thongbaoRoom").hide();
+	$("#messagesRoom").hide();
+	$("#sayHiRoom").hide();
+});
+
+socket.on("server-send-room",function(data){
+	$("#currentRoom").html(data);
+	$("#listMessagesRoom").show(1500);
+	$("#thongbaoRoom").show(1500);
+	$("#messagesRoom").show(1500);
+	$("#listMessages").hide(1000);
+	$("#sayHiRoom").show(1500);		
+	$("#thongbao").hide(1000);
+	$("#messages").hide(1000);
+});
+
+socket.on("server-send-messages",function(data){
+	$("#listMessages").append("<div class='ms'>" + data.un + ":" + data.nd +"</div>");	
+});
+
+socket.on("dang-nhap",function(data){
+	$("#thongbao").html(data);
+});
+
+socket.on("stop-chat",function(){
+	$("#thongbao").html("");
+});
+
+socket.on("room-dang-nhap",function(data){
+	$("#thongbaoRoom").html(data);
+});
+
+socket.on("room-stop-chat",function(){
+	$("#thongbaoRoom").html("");
+});
+
+
+socket.on("server-chat-room",function(data){
+	$("#listMessagesRoom").append("<div class='msRoom'>" + data.un + ":" + data.nd +"</div>");	
+});
+
+$(document).ready(function(){
+	$("#loginForm").show();
+	$("#chatForm").hide();
+
+	//emit username>server
+	$("#btnRegister").click(function(){
+		socket.emit("Client-send-username", $("#txtUserName").val());
+	});
+
+	$("#btnLogout").click(function(){
+		socket.emit("logout");
+		$("#loginForm").show(2000);
+		$("#chatForm").hide(1000);
+	});
+
+	$("#btnSendMessages").click(function(){
+		socket.emit("user-send-messages", $("#txtMessages").val());
+	});
+
+	$("#txtMessages").focusin(function(){
+		socket.emit("chatting");
+	}); 
+
+	$("#txtMessages").focusout(function(){
+		socket.emit("offchat");
+	}); 
+
+	$("#txtMessagesRoom").focusin(function(){
+		socket.emit("room-chatting");
+	}); 
+
+	$("#txtMessagesRoom").focusout(function(){
+		socket.emit("room-offchat");
+	}); 
+
+
+	$("#btnRoom1").click(function(){
+		socket.emit("join-room1");
+	});
+
+	$("#btnLeaveRoom").click(function(){
+		socket.emit("leave-room");
+		$("#listMessagesRoom").hide(1000);
+		$("#thongbaoRoom").hide(1000);
+		$("#messagesRoom").hide(1000);
+		$("#sayHiRoom").hide(1000);	
+		$("#listMessages").show(1500);			
+		$("#thongbao").show(1500);
+		$("#messages").show(1500);
+	})
+
+	$("#btnSendMessagesRoom").click(function(){
+			socket.emit("user-send-messages-room", $("#txtMessagesRoom").val());
+		});
+
+	// $("#btnRoom2").click(function(){
+	// 	socket.emit("join-room2");
+	// });
+
+	// $("#btnRoom3").click(function(){
+	// 	socket.emit("join-room3");
+	// });
+
+
+});
